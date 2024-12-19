9e25e71556dc43e0d1cbcb65ac004ec139b9989e
Anthony Tran <anthonytran@Anthonys-MacBook-Air.local>
2024-12-19 16:00:05 +0100

    adds SumDigits

---
+++ src/PairingKatas/SumDigits.java
@@ -0,0 +1,12 @@
+public class SumDigits {
+    public static int SumDigits(int number) {
+        String numberStr = "" + number;
+        for(int i=0; i<numberStr.length(); i++){
+            System.out.println(numberStr.charAt(i));
+        }
+        return -1;
+    }
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World");
+    //   }
+}
