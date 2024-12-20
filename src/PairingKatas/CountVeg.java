9bfac6e13dba9d19665be9410bcb5c26cb9255df
anthonytrann <anthonytran227@gmail.com>
2024-12-20 10:53:16 +0100

    initial testing for CountVeg

---
+++ src/PairingKatas/CountVeg.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class CountVeg {
+    public static int getCountVeg(ArrayList<HashMap<String, Object>> vegList, String type) {
+        return -1;
+    }
+}
---
+++ test/TestPairingKatas/TestCountVeg.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList; 
+import java.util.HashMap;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TestCountVeg {
+    @Test
+    public void takesListOfDictionaryReturnsNothing(){
+        HashMap<String, Object> vegDict1 = new HashMap<>();
+        vegDict1.put("name", "Parsnip");
+        vegDict1.put("type", "root");
+        vegDict1.put("quantity", 4);
+
+        HashMap<String, Object> vegDict2 = new HashMap<>();
+        vegDict2.put("name", "Broccoli");
+        vegDict2.put("type", "brassica");
+        vegDict2.put("quantity", 1);
+
+        HashMap<String, Object> vegDict3 = new HashMap<>();
+        vegDict3.put("name", "Carrot");
+        vegDict3.put("type", "root");
+        vegDict3.put("quantity", 5);
+
+        HashMap<String, Object> vegDict4 = new HashMap<>();
+        vegDict4.put("name", "Onion");
+        vegDict4.put("type", "bulb");
+        vegDict4.put("quantity", 3);
+
+        ArrayList<HashMap<String, Object>> vegList = new ArrayList<>();
+        vegList.add(vegDict1);
+        vegList.add(vegDict2);
+        vegList.add(vegDict3);
+        vegList.add(vegDict4);
+
+        int expectedOutput = 9;
+        int result = CountVeg.getCountVeg(vegList, "root");
+        assertEquals(expectedOutput, result, 0);
+    }
+}
