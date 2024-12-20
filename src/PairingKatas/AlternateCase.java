03443eda6cd463c3d29f1fe5375764602ee4751f
anthonytrann <anthonytran227@gmail.com>
2024-12-20 11:48:58 +0100

    completes alternate case for non-alphabet chars

--- src/PairingKatas/AlternateCase.java
+++ src/PairingKatas/AlternateCase.java
@@ -1,10 +1,13 @@
 public class AlternateCase {
-    public static String getAlternateCase(String sentence){
+    public static String getAlternateCase(String sentence) {
         String newSentence = "";
         Boolean capital = true;
-        for(int i = 0 ; i < sentence.length(); i++){
+        for(int i = 0 ; i < sentence.length(); i++) {
             char c = sentence.charAt(i);
-            if(capital == true){
+            if(!Character.isLetter(c)) {
+                newSentence += c;
+                
+            } else if(capital == true) {
                 newSentence += Character.toUpperCase(c);
                 System.out.println(newSentence);
                 capital = false;
