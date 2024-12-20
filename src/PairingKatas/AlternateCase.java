018f5925df364428c3065c59fd6f164a9f391b4a
anthonytrann <anthonytran227@gmail.com>
2024-12-20 11:45:54 +0100

    adds testing for one word

--- src/PairingKatas/AlternateCase.java
+++ src/PairingKatas/AlternateCase.java
@@ -1,5 +1,18 @@
 public class AlternateCase {
     public static String getAlternateCase(String sentence){
-        return "";
+        String newSentence = "";
+        Boolean capital = true;
+        for(int i = 0 ; i < sentence.length(); i++){
+            char c = sentence.charAt(i);
+            if(capital == true){
+                newSentence += Character.toUpperCase(c);
+                System.out.println(newSentence);
+                capital = false;
+            } else {
+                newSentence += c;
+                capital = true;
+            }
+        }
+        return newSentence;
     }
 }
--- test/TestPairingKatas/TestAlternateCase.java
+++ test/TestPairingKatas/TestAlternateCase.java
@@ -3,9 +3,23 @@ import static org.junit.Assert.assertEquals;
 
 public class TestAlternateCase {
     @Test
-    public void emptySring(){
+    public void emptyString(){
         String expectedOutput = "";
         String result = AlternateCase.getAlternateCase("");
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void oneWord(){
+        String expectedOutput = "HeLlO";
+        String result = AlternateCase.getAlternateCase("hello");
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void withSpace(){
+        String expectedOutput = "HeLlO wOrLd";
+        String result = AlternateCase.getAlternateCase("hello world");
         assertEquals(expectedOutput, result);
     }
 }
