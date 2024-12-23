48dd4792c3ce0d07233630ec4cdf80e2c8a273d1
anthonytrann <anthonytran227@gmail.com>
2024-12-23 12:59:28 +0100

    completes main section of report_the_grand_goat_race

--- src/main/pairing_katas/ReportTheGrandGoatRace.java
+++ src/main/pairing_katas/ReportTheGrandGoatRace.java
@@ -1,9 +1,69 @@
-
 import java.util.HashMap;
 
 public class ReportTheGrandGoatRace {
     public static HashMap<String, String> report(String goatInfo) {
         HashMap<String, String> result = new HashMap<>();
+        String[] goatSplit = goatInfo.split(",");
+        result.put("first_place", "");
+        result.put("goat_with_longest_name", "Bleaters");
+        result.put("average_time", "00:30:00");
+
+        int fastestTime = 0;
+        String firstPlace = "";
+        String longestName = "";
+        int totalTime = 0;
+        for(int i = 0; i < goatSplit.length; i++) {
+            String goat = goatSplit[i];
+            System.err.println(goat);
+            String[] information = goat.split(": ");
+            String name = information[0].strip();
+            int timeTaken = timeToSeconds(information[1]);
+            System.err.println(name);
+            System.err.println(timeTaken);
+            if(i == 0) {
+                fastestTime = timeTaken;
+                firstPlace = name;
+                longestName = name;
+                totalTime += timeTaken;
+            } else {
+                if(timeTaken < fastestTime) {
+                    fastestTime = timeTaken;
+                    firstPlace = name;
+                }
+    
+                if(name.length() > longestName.length()) {
+                    longestName = name;
+                }
+    
+                totalTime += timeTaken;    
+            }
+        }
+        int averageTime = (totalTime/goatSplit.length);
+        result.put("first_place", firstPlace);
+        result.put("goat_with_longest_name", longestName);
+        result.put("average_time", secondsToTime(averageTime));
+
         return result;
+    }
+
+    public static int timeToSeconds(String time) {
+        String[] h1 = time.split(":");
+         
+        int hour = Integer.parseInt(h1[0]);
+        int minute = Integer.parseInt(h1[1]);
+        int second = Integer.parseInt(h1[2]);
+         
+        int temp;
+        temp = second + (60 * minute) + (3600 * hour);
+        return temp;
+    }
+
+    public static String secondsToTime(int time) {
+
+        int hours = time / 3600;
+        int remainingSeconds = time % 3600;
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        return String.format("%02d:%02d:%02d", hours, minutes, seconds);
     }
 }
--- src/test/TestPairingKatas/TestReportTheGrandGoatRace.java
+++ src/test/TestPairingKatas/TestReportTheGrandGoatRace.java
@@ -3,6 +3,7 @@ import java.util.HashMap;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class TestReportTheGrandGoatRace {
+    @Test
     public void testOneGoat() {
         // String goatInfo = "Bleaters: 00:30:00, Professor Dandelion: 01:00:00, Katherine Jr.: 00:45:00";
         String goatInfo = "Bleaters: 00:30:00,";
@@ -10,6 +11,33 @@ public class TestReportTheGrandGoatRace {
         expectedOutput.put("first_place", "Bleaters");
         expectedOutput.put("goat_with_longest_name", "Bleaters");
         expectedOutput.put("average_time", "00:30:00");
+
+        HashMap<String,String> result = ReportTheGrandGoatRace.report(goatInfo);
+
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void testTwoGoats() {
+        // String goatInfo = "Bleaters: 00:30:00, Professor Dandelion: 01:00:00, Katherine Jr.: 00:45:00";
+        String goatInfo = "Bleaters: 00:30:00, Professor Dandelion: 01:00:00,";
+        HashMap<String,String> expectedOutput = new HashMap<>();
+        expectedOutput.put("first_place", "Bleaters");
+        expectedOutput.put("goat_with_longest_name", "Professor Dandelion");
+        expectedOutput.put("average_time", "00:45:00");
+
+        HashMap<String,String> result = ReportTheGrandGoatRace.report(goatInfo);
+
+        assertEquals(expectedOutput, result);
+    }
+
+    @Test
+    public void moreGoats() {
+        String goatInfo = "Bleaters: 00:30:00, Professor: 01:00:00, Katherine Jr.: 00:45:00";
+        HashMap<String,String> expectedOutput = new HashMap<>();
+        expectedOutput.put("first_place", "Bleaters");
+        expectedOutput.put("goat_with_longest_name", "Katherine Jr.");
+        expectedOutput.put("average_time", "00:45:00");
 
         HashMap<String,String> result = ReportTheGrandGoatRace.report(goatInfo);
 
